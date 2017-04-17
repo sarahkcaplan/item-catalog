a856e69e619aea2bb272fbc51f1c2e691d208434
Sarah Caplan <caplan.sarah@gmail.com>
2017-04-17 12:52:41 -0600

    Setup config

---
+++ database_setup.py
@@ -0,0 +1,23 @@
+import sys
+
+from sqlalchemy import
+Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import
+declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+
+
+### Goes at end of file
+engine = create_engine(
+	'sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
