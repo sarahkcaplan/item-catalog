fc69042d8e8eb68b175e1cb478a67706a794b1a3
Sarah Caplan <caplan.sarah@gmail.com>
2017-04-18 23:46:24 -0600

    Removed config lines because not needed as they are in the populator script

--- puppies_db_setup.py
+++ puppies_db_setup.py
@@ -7,49 +7,29 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-
-
 class Shelter(Base):
-	__tablename__ = 'shelter'
-
-	name = Column(String(80), nullable = False)
-
-	address = Column(String(250), nullable = False)
-
-	city = Column(String(80), nullable = False)
-
-	state = Column(String(80), nullable = False)
-
-	zipCode = Column(Integer, nullable = False)
-
-	website = Column(String(250))
-
-	id = Column(Integer, primary_key = True)
-
+    __tablename__ = 'shelter'
+    id = Column(Integer, primary_key = True)
+    name =Column(String(80), nullable = False)
+    address = Column(String(250))
+    city = Column(String(80))
+    state = Column(String(20))
+    zipCode = Column(String(10))
+    website = Column(String)
 
 class Puppy(Base):
-	__tablename__ = 'puppy'
-
-	name = Column(String(80), primary_key = True)
-
-	dob = Column(String(80), nullable = False)
-
-	gender = Column(String(80), nullable = False)
-
-	weight = Column(Integer, nullable = False)
-
-	shelter_id = Column(Integer, ForeignKey('shelter.id'))
-
-	photo = Column(LargeBinary)
+    __tablename__ = 'puppy'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(6), nullable = False)
+    dateOfBirth = Column(Date)
+    picture = Column(String)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
+    weight = Column(Numeric(10))
 
 
 engine = create_engine('sqlite:///puppies.db')
-
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-
-session = DBSession()
 
 Base.metadata.create_all(engine)
 
